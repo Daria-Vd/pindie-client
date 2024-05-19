77c6f0e47e654aec0bf2923af5db404a448c2d56
Dariya Vedernikova <daria-vedernikova08@yandex.ru>
2024-05-19 08:40:25 +0300

    fix: change base url

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://server.nomoredomainswork.ru";
+export const BASE_URL = "https://server.nomoredomainswork.ru/api";
 
 export const endpoints = {
     games: `${BASE_URL}/games`,
